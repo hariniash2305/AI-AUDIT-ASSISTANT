7ab961792cb0ec215176378c5c4082c90a4b91d3
Harini Ashok <hariniash2305@gmail.com>
2026-06-11 19:37:12 +0530

    Implement PDF upload and data extraction
    Add PDF upload functionality and extract invoice details.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, UploadFile, File
+import pdfplumber
+import os
 from fastapi.middleware.cors import CORSMiddleware
-
+import re
 
 app = FastAPI()
 
@@ -12,11 +14,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.get("/")
 def home():
-    return "Home"
+    return {"message": "AI Audit API Running"}
 
 @app.post("/upload")
-async def upload_file(file: UploadFile = File(...)):
-    return {"message": "File uploaded successfully"}
+async def upload_pdf(file: UploadFile = File(...)):
+
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+
+    text = ""
+
+    with pdfplumber.open(file_path) as pdf:
+        for page in pdf.pages:
+            page_text = page.extract_text()
+
+            if page_text:
+                text += page_text + "\n"
+
+    invoice_match = re.search(r"Invoice Number:\s*(.+)", text)
+    vendor_match = re.search(r"Vendor:\s*(.+)", text)
+    amount_match = re.search(r"Amount:\s*[₹Rs.\s]*([\d,]+)", text)
+    date_match = re.search(r"Date:\s*(\d{2}/\d{2}/\d{4})", text)
+
+    invoice = invoice_match.group(1).strip() if invoice_match else None
+    vendor = vendor_match.group(1).strip() if vendor_match else None
+    amount = amount_match.group(1).replace(",", "") if amount_match else None
+    date = date_match.group(1) if date_match else None
+
+    return {
+        "invoice": invoice,
+        "vendor": vendor,
+        "amount": amount,
+        "date": date
+    }
